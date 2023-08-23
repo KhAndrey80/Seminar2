423f4374dfbba967c9ffacfdb2847de36258c40d
Andrey <A.com>
2023-08-23 23:32:53 +0300

    2

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу которая принимает число N и выдает таблицу квадратов чисел от 1 до N
+System.Console.WriteLine("Введите положительное число");
+int N = Convert.ToInt32(Console.ReadLine());
+int S = 1;
+int qw = 0;
+while (S < N+1 ) 
+{
+qw = S * S;
+System.Console.WriteLine(qw);
+S++;
+}
