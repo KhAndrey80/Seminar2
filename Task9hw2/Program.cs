be94bb2bf9cffd6af6a5d05fb5a10d40f5f62a6e
Andrey <A.com>
2023-08-26 12:54:19 +0300

    5

---
+++ Task9hw2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу которая принимает число N и выдает таблицу кубов чисел от 1 до N. 3-1,8,27
+System.Console.WriteLine("Введите положительное число");
+int N = Convert.ToInt32(Console.ReadLine());
+int S = 1;
+while (S <= N) 
+{
+System.Console.WriteLine(S + "  " + S*S*S);
+S++;
+}
