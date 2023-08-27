12ed16f7b468ad7d3ad0de2e88a479544310082d
Andrey <A.com>
2023-08-27 13:15:15 +0300

    7

---
+++ Task8hw1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Принимает 5 значное число и проверяет, является ли оно полиндромом. 12321 -да, 12125 - нет
+System.Console.WriteLine("Введите пятизначное число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int N1 = (N - N %10000) / 10000;
+int N2 = ((N-N %1000) %10000) / 1000;
+int N4 = (N %100 - N %10) / 10;
+int N5 = N %10;
+
+if (N1 == N5 && N2 == N4)
+
+{
+System.Console.WriteLine("Полиндром");
+}
+else 
+{
+    System.Console.WriteLine("Не полиндром");
+}
+
+System.Console.WriteLine(N %10);//5 число
+System.Console.WriteLine((N %100 - N %10) / 10);// 4 число
+System.Console.WriteLine((N - N %10000) / 10000);// 1 число
+System.Console.WriteLine(((N-N %1000) %10000) / 1000); //2 число
