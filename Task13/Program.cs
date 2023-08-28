58b3634bdb5f0c9056b2af5d03b12fbf01898ff2
Andrey <A.com>
2023-08-28 17:06:08 +0300

    9

---
+++ Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿//Программа принимает на вход число N и выдает произведение чисел от 1 до N
+int Parse(string text) 
+{
+    System.Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int Proizv (int number)
+{
+    int proizv = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        proizv = proizv * i;
+    }
+    return proizv;
+}
+
+int number = Parse("Введите число");
+int proizv = Proizv (number);
+System.Console.WriteLine(proizv);
