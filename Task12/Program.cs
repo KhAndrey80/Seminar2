82311672b3c946e3d33fb3293b0af600e4a57280
Andrey <A.com>
2023-08-28 16:58:29 +0300

    9

---
+++ Task12/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число и выдвет кол-во цифр в числе.
+using System.Reflection.Metadata.Ecma335;
+
+int GetNumber() 
+{
+    System.Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int CountElems (int number)
+{
+   if (number < 0)
+   {
+    number = number * -1;
+   } 
+   int count = 0;
+   while (number > 0)
+   {
+    number = number / 10;
+   count++;
+   }
+    return count;
+}
+
+
+int number = GetNumber();
+int sum = CountElems (number);
+System.Console.WriteLine(sum);
