81bd32abd55d60813af0292f8d901d94b62dd549
Andrey <A.com>
2023-08-29 21:06:14 +0300

    s4hw3

---
+++ Task17/Program.cs
@@ -0,0 +1,23 @@
+﻿//Программа, которая задает массив из 8 элементов и выводит на экран.
+int [] array = new int[8];
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    string arrayOutput = String.Join(", ", array);
+    System.Console.WriteLine("[" + arrayOutput + "]");
+}
+
+FillArray(array);
+PrintArray(array);
+
