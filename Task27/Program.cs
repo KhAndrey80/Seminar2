f47f73241812b14e610d50018b28be18d2b949bf
Andrey <A.com>
2023-09-03 00:01:55 +0300

    s6z5

---
+++ Task27/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+Console.Write("Введите десятичное число: ");
+int decimalNumber = int.Parse(Console.ReadLine());
+string binaryString = string.Empty;
+while (decimalNumber > 0)
+{
+int remainder = decimalNumber % 2;
+decimalNumber /= 2;
+binaryString = remainder.ToString() + binaryString;
+}
+Console.WriteLine($"Двоичное представление числа: {binaryString}");
