1722248460ede8f015a4df706b6f8b2991e045a2
Andrey <A.com>
2023-09-09 16:28:23 +0300

    s87z1

---
+++ Task35/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+
+int[,] array = new int[4, 7];
+
+int[,] FillArray(int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+array[i, j] = new Random().Next(-10, 10);
+Console.Write(array[i, j] + "\t");
+}
+Console.WriteLine();
+}
+return array;
+}
+
+int[,] array2 = FillArray(array);
+
+Console.WriteLine("---------------");
+
+void FillArray21(int[,] array)
+{
+int res = 0;
+for (int i = 0; i < array.GetLength(1); i++)
+{
+res = array[0, i];
+array[0, i] = array[array.GetLength(0) - 1, i];
+array[array.GetLength(0) - 1, i] = res;
+}
+Console.WriteLine();
+
+}
+
+FillArray21(array2);
+
+void PrintArray(int[,] array){
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+Console.Write(array[i, j] + "\t");
+}
+Console.WriteLine();
+}
+}
+
+PrintArray(array2);
