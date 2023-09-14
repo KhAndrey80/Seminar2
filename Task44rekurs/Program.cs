853f2a9789a742a141b2ff9c1584a60f760ba448
Andrey <A.com>
2023-09-14 22:24:19 +0300

    s9z3

--- Task44rekurs/Program.cs
+++ Task44rekurs/Program.cs
@@ -15,13 +15,27 @@
 // 2. Написать программу, которая принимает число и возвращает сумму его цифр.
 
 
-int SumNumber(int number)
+// int SumNumber(int number)
+// {
+//     if (number < 10)
+// {
+//     return number;
+// }
+// return number %10 + SumNumber(number /10);
+// }
+
+// System.Console.WriteLine(SumNumber(453));
+
+//3. Написать программу, которая принимает 2 числа А и В и возводит А в степеь В.
+// А=3, В=5, 3^5 = 243
+
+int Stepen(int a, int b)
+ {
+     if (b == 1)
 {
-    if (number == 0)
-{
-    return 0;
+     return a;
 }
-return number %10 + SumNumber(number /10);
+return a * Stepen(a, b-1);
 }
 
-System.Console.WriteLine(SumNumber(453));
+System.Console.WriteLine(Stepen(3,5));
